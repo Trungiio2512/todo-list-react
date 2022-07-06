176c5be09d4a754a57672bd53329cc726c3b82e8
Trungiio <91168572+Trungiio2512@users.noreply.github.com>
2022-07-06 21:55:09 +0700

    not

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [job, setJob] = useState(() => "");
+  const [jobs, setJobs] = useState(() => {
+    return JSON.parse(localStorage.getItem("jobs")) ?? [];
+  });
+
+  const hanldeSubmit = () => {
+    setJobs((prev) => {
+      const StorageJob = [...prev, job];
+      localStorage.setItem("jobs", JSON.stringify(StorageJob));
+
+      setJob("");
+
+      return StorageJob;
+    });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <ul>
+        <input
+          value={job}
+          onChange={(e) => {
+            setJob(e.target.value);
+          }}
+        />
+        <button onClick={hanldeSubmit}>Add</button>
+        {jobs.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
